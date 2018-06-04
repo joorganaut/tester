7f5a448b08c85e5d81ef3f87b53d20bc9e1be753
Osazee Igbinosun <Osazee.Igbinosun@zenithbank.com>
2018-06-04 19:38:00 +0100

    first

---
+++ Transaction.cs
@@ -0,0 +1,24 @@
+﻿using NIP.Adhoc.Core.contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NIP.Adhoc.Core.data
+{
+    [Serializable]
+
+    public class Transaction : IBusinessObject
+    {
+        public DateTime DateCreated { get; set; }
+        public string ResponseCode { get; set; }
+        public string AccountNo { get; set; }
+        public string SessionID { get; set; }
+        public decimal Amount { get; set; }
+        public string BankName { get; set; }
+        public string Channel { get; set; }
+        public long ID { get; set; }
+        public int Status { get; set; }
+    }
+}
